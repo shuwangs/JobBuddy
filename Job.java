94d6ad69a62223c3359a4de2fa641017c292eef6
shu.wang <60482034+shuwangs@users.noreply.github.com>
2025-11-26 19:39:56 +0000

    add ClassDiagram reoganize folder structure.

---
+++ Job.java
@@ -0,0 +1,38 @@
+    // - Long id
+    // - String title
+    // - String company
+    // - String url
+    // - String location
+    // - String requisitionNumber 
+    // - Date postedDate
+    // - String salaryRange
+    // - JobStatus jobStatus
+    // - String notes
+    // - Date createdAt
+    // - Date updatedAt
+    // + getId(): Numeric
+    // + getTitle(): String
+    // + getCompany(): String
+    // + getUrl(): String
+    // + getLocation(): String
+    // + getRequisitionNumber(): String
+    // + getSalaryRange(): String
+    // + getPostedDate(): Date
+    // + getJobStatus(): JobStatus
+    // + getNotes(): String
+    // + getCreatedAt(): Date
+    // + getUpdatedAt(): Date      
+
+    // + setTitle(title: String): void
+    // + setCompany(company: String): void
+    // + setUrl(url: String): void
+    // + setLocation(location: String): void
+    // + setRequisitionNumber(requisitionNumber: String): void
+    // + setSalaryRange(salaryRange: String): void
+    // + setPostedDate(postedDate: Date): void
+    // + setJobStatus(jobStatus: JobStatus): void
+    // + setNotes(notes: String): void
+    // + setCreatedAt(createdAt: Date): void
+    // + setUpdatedAt(updatedAt: Date): void
+    // + save(): void
+    // + delete(): void

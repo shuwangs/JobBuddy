b71a2c4ec55f72c9a1aedf0d82cefb6b23d9fc9f
Shu <shuw425@gmail.com>
2025-12-12 02:53:27 -0500

    comments default JobRepository interface:

--- backend/src/main/java/com/jobbuddy/backend/repository/JobRepository.java
+++ backend/src/main/java/com/jobbuddy/backend/repository/JobRepository.java
@@ -1,9 +1,14 @@
 package com.jobbuddy.backend.repository;
+import com.jobbuddy.backend.model.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JobRepository {
-//    + save(job: Job): Job
-//    + findById(id: Long): Optional<Job>
-//    + findAll(): List<Job>
-//    + deleteById(id: Long): void
+public interface JobRepository extends JpaRepository<Job, Long> {
+    // JpaRespository commonly used methods
+    // saveAll(): Saves all given entities.
+    // getById(): Saves all given entities.
+    // flush(): Flushes all pending changes to the database.
+    // saveAndFlush(): save an entity and flushes changes
+    // deleteAllInBatch(): Deletes multiple entities in a single batch query.
+
 
 }

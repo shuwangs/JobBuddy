9c5dd58c0bf80d5d034089c78c6ff0c31c3229ca
Shu <shuw425@gmail.com>
2025-12-12 03:08:57 -0500

    add JobService

--- backend/src/main/java/com/jobbuddy/backend/service/JobService.java
+++ backend/src/main/java/com/jobbuddy/backend/service/JobService.java
@@ -1,4 +1,41 @@
 package com.jobbuddy.backend.service;
+import com.jobbuddy.backend.model.Job;
+import com.jobbuddy.backend.model.JobStatus;
+import com.jobbuddy.backend.repository.JobRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class JobService {
+    private JobRepository jobRepo;
+
+    public JobService(JobRepository JobRepository) {
+        this.jobRepo = jobRepo;
+    }
+
+    public List<Job> listJobs(JobStatus status) {
+        return jobRepo.findAll();
+    }
+
+    public Job getJobById(Long id) {
+        return jobRepo.findById(id).orElse(null);
+    }
+
+    public void createJob(Job job) {
+        jobRepo.save(job);
+    }
+
+    public void deleteJobById(Long id) {
+        jobRepo.deleteById(id);
+    }
+
+    public Job updateJob(Long id, JobStatus status) {
+        Job job = jobRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Job not found"));
+        job.setStatus(status);
+        return jobRepo.save(job);
+    }
+
 }

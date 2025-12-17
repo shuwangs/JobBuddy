f23b209b107410ab65623bb271f4ee2c06707f28
shu.wang <60482034+shuwangs@users.noreply.github.com>
2025-12-17 17:58:54 +0000

    initialize frontend

--- backend/src/main/java/com/jobbuddy/backend/config/WebConfig.java
+++ backend/src/main/java/com/jobbuddy/backend/config/WebConfig.java
@@ -8,7 +8,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Adjust this to your frontend's URL
+                .allowedOrigins("http://localhost:5137") // Adjust this to your frontend's URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
                 // .allowedHeaders("*")
                 // .allowCredentials(true);

ff43dd7ac13fdfe6e98891eb34fad260033bf39e
shu.wang <60482034+shuwangs@users.noreply.github.com>
2025-12-17 17:54:20 +0000

    add webConfig to across CORS

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JobBuddy
 JobBuddy is a lightweight tool that helps job seekers track applications, monitor status updates, and manage their job search efficiently.
+
+
---
+++ backend/src/main/java/com/jobbuddy/backend/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.jobbuddy.backend.config;
+import org.springframework.context.annotaiton.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Adjust this to your frontend's URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+                // .allowedHeaders("*")
+                // .allowCredentials(true);
+    };
+}

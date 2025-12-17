8236b5d52323dfa43538935660cd14c246635397
shu.wang <60482034+shuwangs@users.noreply.github.com>
2025-12-17 19:35:13 +0000

    add a little in the navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className='navbar bg-light mb-4'>
+            JobBuddy
+        </nav>
+    )
+}
+export default Navbar;

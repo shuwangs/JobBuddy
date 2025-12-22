149f39813789e6547bad78bd8624090fe6256bf7
Shu <shuw425@gmail.com>
2025-12-21 20:54:00 -0500

    fix: addjob posted to deployed backend

--- frontend/src/pages/AddJob.jsx
+++ frontend/src/pages/AddJob.jsx
@@ -44,7 +44,7 @@ const AddJob = () => {
 
         if (!jobToSave) return;
         // TODO: Call API to save
-        await axios.post('http://localhost:8081/api/jobs', jobToSave)
+        await axios.post('`${process.env.VITE_API_BASE_URL}/api/jobs`/api/jobs', jobToSave)
         console.log("saved successfully!");
         clearInputArea();
     } catch (error) {

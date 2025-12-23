bb6308858f99ac431e8a5272eddccb51a9c93678
shu.wang <60482034+shuwangs@users.noreply.github.com>
2025-12-23 20:35:14 +0000

    add possible linkedparser not tested

--- extension/content.js
+++ extension/content.js
@@ -17,5 +17,32 @@ const getJobDetails = () => {
 }
 
 const extractLinkedInJobDetails = () => {};
-const extractIndeedJobDetails = () => {};
+const extractIndeedJobDetails = (url) => {
+    let title = document.querySelector('h1')?.innerText 
+           || document.querySelector('.jobsearch-JobInfoHeader-title')?.innerText
+           || "Unknown Title";
+
+    let company = document.querySelector('[data-company-name="true"]')?.innerText
+             || document.querySelector('.jobsearch-CompanyInfoContainer a')?.innerText
+             || "Unknown Company";
+
+    let location = document.querySelector('[data-testid="inlineHeader-companyLocation"]')?.innerText
+              || document.querySelector('.jobsearch-JobInfoHeader-subtitle div:last-child')?.innerText
+              || "";
+    let salary = document.querySelector('#salaryInfoAndJobType')?.innerText
+            || document.querySelector('.jobsearch-JobMetadataHeader-item')?.innerText
+            || "";
+
+    const jobPayload = {
+    title: title,
+    company: company,
+    location: location,
+    salaryRange: salaryRaw,  
+    url: url,
+    status: "WAITLISTED",    
+    description: description.substring(0, 500)
+  };
+
+  return jobPayload;
+};
 const extractGlassdoorJobDetails = () => {};

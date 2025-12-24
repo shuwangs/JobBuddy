ecd2fe1e433cf3f2a6054f672a1f9bdb82a66f00
Shu <shuw425@gmail.com>
2025-12-24 00:50:56 -0500

    add eventListen for the saveBtn

--- extension/popup.js
+++ extension/popup.js
@@ -1,1 +1,17 @@
-// connect the content script to the backend db
+// Get the current active url
+document.getElementById('parseBtn').addEventListener('click', 
+    async ()=> {
+        const [tab] = await chrome.tabs.query({
+            active: true, currentWindow: true
+        })
+        if(tab.id) {
+            chrome.tabs.sendMessage(tab.id, { action: "SAVE_JOB" }, (response) => {
+            if (chrome.runtime.lastError) {
+                console.error("Conection failed:", chrome.runtime.lastError.message);
+            } else {
+                console.log("Message sent，Content Script responded:", response);
+            }
+            });
+        }
+    }
+)

4b003fe4fc149cb0748e585c62f5100f45e4eb3c
Shu <shuw425@gmail.com>
2025-12-27 00:48:59 -0500

    feat(backend): implement CustomUserDetailsService for auth - Create `CustomUserDetailsService` implementing Spring's `UserDetailsService`. - Inject `UserRepository` to retrieve user data from database. - Handle `UsernameNotFoundException` when user does not exist.

---
+++ backend/src/main/java/com/jobbuddy/backend/service/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.jobbuddy.backend.service;
+import com.jobbuddy.backend.model.Job;
+import com.jobbuddy.backend.model.User;
+import com.jobbuddy.backend.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                Collection.emptyList()
+        );
+    }
+}

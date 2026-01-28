07bd1e2af4157c7e80bb7ef597de59f6fc4def87
Shu <shuw425@gmail.com>
2026-01-28 01:38:58 -0500

    feat: add a landingpage

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+
+const LandingPage = () => {
+    
+    return (
+        <div>
+            <div>
+                <h1>JobBuddy</h1>
+                <h3>Your smart companion for job tracking & applications</h3>
+
+                <p>JobBuddy helps you collect, organize, and track job applications 
+                in one place — so you never lose track of opportunities, deadlines, or notes.</p>
+
+            </div>
+
+            <div>
+                <h3>Why JobBuddy?</h3>
+                - Applying for jobs often means juggling:
+
+                - Multiple job boards
+
+                - Countless tabs and spreadsheets
+
+                - Scattered notes and follow-ups
+
+                - JobBuddy brings everything together.
+            </div>
+
+            <div>
+                <h3>Ready to get organized?</h3>
+                <button>Get Started</button>
+                <button> Log In</button>
+            </div>
+            
+        </div>
+    )
+}
+
+export default LandingPage;
